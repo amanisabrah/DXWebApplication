006b8bfe9d1cce69c9f6166a71103858a4d4094e
USER <USER@USERW11>
2024-04-23 16:50:22 +0300

    Add project files.

---
+++ DXWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using DXWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DXWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AccountingDbContext _accountingDbContext;
+
+        public HomeController()
+        {
+            _accountingDbContext = new AccountingDbContext();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<JOB_JOBS> jobs = JOB_JOBS.Get(_accountingDbContext);
+            return View(jobs);
+        }
+
+        [HttpPost]
+        public ActionResult PartialJobsGridView()
+        {
+            List<JOB_JOBS> jobs = JOB_JOBS.Get(_accountingDbContext);
+            return PartialView("_PartialJobsGridView",jobs);
+        }
+
+        [HttpPost]
+        public ActionResult PartialJobsGridViewAddNew()
+        {
+            List<JOB_JOBS> jobs = JOB_JOBS.Get(_accountingDbContext);
+            return PartialView("_PartialJobsGridView",jobs);
+        }
+    }
+}
---
+++ DXWebApplication/Models/DBRead/JOB_JOBS.cs
@@ -0,0 +1,23 @@
+﻿using DXWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DXWebApplication.Models
+{
+    public partial class JOB_JOBS
+    {
+        public static List<JOB_JOBS> Get(AccountingDbContext _dbContext)
+        {
+            return _dbContext.JOB_JOBS.ToList();
+        }
+
+        public enum Gender
+        {
+            Female,
+            Male
+        }
+
+    }
+}
