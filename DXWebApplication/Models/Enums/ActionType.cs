45a669940128dfa50e5164d70f86769077b415d9
Your Name <amanisabrah@gmail.com>
2024-05-15 12:22:13 +0300

    add TypeAction enum class

---
+++ DXWebApplication/Models/Enums/ActionType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DXWebApplication.Models
+{
+    public class ActionType
+    {
+        public static Dictionary<string, int> TypeAction 
+        {
+            get
+            {
+                var FontSize = new Dictionary<string, int>();
+                foreach (var text in Enum.GetNames(typeof(Gender)))
+                {
+                    FontSize.Add(text, (int)Enum.Parse(typeof(Gender), text));
+                }
+                return FontSize;
+            }
+        }
+    }
+    public enum TypeAction
+    {
+        New = 1,
+        Renew = 2
+    }
+}
